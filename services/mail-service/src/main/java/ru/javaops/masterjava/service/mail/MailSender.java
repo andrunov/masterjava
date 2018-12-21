ab8243393a2b3f0c15422d2a348ec919cdf3626c
Andrunov <andrunov@gmail.com>
2018-12-21 17:25:16 +0300

    email via config

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
@@ -1,5 +1,6 @@
 package ru.javaops.masterjava.service.mail;
 
+import com.typesafe.config.Config;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.HtmlEmail;
@@ -12,20 +13,23 @@ public class MailSender {
     static void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) {
         log.info("Send mail to \'" + to + "\' cc \'" + cc + "\' subject \'" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
         HtmlEmail email = new HtmlEmail();
-        email.setHostName("smtp.yandex.ru");
-        email.setSmtpPort(465);
-        email.setAuthentication("Andrunov@yandex.ru", "Andro5775382");
-        email.setSSL(true);
-        email.setTLS(false);
-        email.setDebug(true);
+        Config config = Configs.getConfig("mail.conf", "mail");
+        email.setHostName(config.getString("host"));
+        email.setSmtpPort(config.getInt("port"));
+        email.setAuthentication(config.getString("username"), config.getString("password"));
+        email.setSSL(config.getBoolean("useSSL"));
+        email.setTLS(config.getBoolean("useTLS"));
+        email.setDebug(config.getBoolean("debug"));
         try {
-            email.setFrom("Andrunov@yandex.ru", "Andro");
+            email.setFrom(config.getString("username"), config.getString("fromName"));
             for (Addressee addressee : to) {
                 email.addTo(addressee.getEmail(), addressee.getName());
             }
             for (Addressee addressee : cc) {
                 email.addCc(addressee.getEmail(), addressee.getName());
             }
+            email.setSubject(subject);
+            email.setHtmlMsg(body);
             email.send();
 
         } catch (EmailException e) {
