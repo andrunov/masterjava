baa6383688157360884cc6fd31eeae1193482de8
Gorbunov <andrunov@gmail.com>
2018-12-28 23:54:31 +0300

    mail service wsdl repair2

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -5,7 +5,7 @@ import com.google.common.collect.ImmutableSet;
 public class MailWSClientMain {
     public static void main(String[] args) {
         MailWSClient.sendToGroup(
-                ImmutableSet.of(new Addressee("To <masterjava@javaops.ru>")),
-                ImmutableSet.of(new Addressee("Copy <masterjava@javaops.ru>")), "Subject", "Body");
+                ImmutableSet.of(new Addressee("To <andrunov@gmail.com>")),
+                ImmutableSet.of(new Addressee("Copy <agorbunov@integrotechnologies.ru>")), "Subject 321", "Body 123");
     }
 }
